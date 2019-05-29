b958dcabbe756d0ebfc0e04a6ba7d9e35478d565
Thiebaud <contact@communnicasciences.eu>
2019-05-29 13:06:14 +0200

    integration gardien

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ function preload ()
     this.load.image('player', 'assets/player.png');
     this.load.image('background', 'assets/background.png');
     this.load.image('tresor', 'assets/treasure.png');
+    this.load.image('dragon', 'assets/dragon.png');
 }
 
 function create ()
@@ -30,6 +31,7 @@ function create ()
     var joueur = this.add.image(20, 300, 'player');
     joueur.setScale (0.7);
     this.add.image(725, 145, 'tresor').setScale(0.7);
+    this.add.image(500, 120, 'dragon').setScale(0.7);
 }
 
 function update ()
