34460f4d5d5bbf6b8960473f61c39de33abadebd
mamapoux <marine.schwartz@outlook.fr>
2019-05-29 11:18:11 +0200

    background

--- js/main.js
+++ js/main.js
@@ -4,18 +4,24 @@ var config = {
     type: Phaser.AUTO,
     width:800,
     height:600,
+
+    scene: {
+        preload : preload,
+        create : create,
+        update: update,
+    }
 };
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-
+    this.load.image('background', 'assets/background.png');
 }
 
 function create ()
 {
-
+    this.add.sprite(400, 300, 'background');
 }
 
 function update ()
