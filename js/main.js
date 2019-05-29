b9a13ee6bb27451935bfabfe54c888d254c4f456
mamapoux <marine.schwartz@outlook.fr>
2019-05-29 10:37:02 +0200

    initialisation

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var scene = new Phaser.Scene ("jeu");
+
+var config = {
+    type: Phaser.AUTO,
+    width:800,
+    height:600,
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+}
+
+function create ()
+{
+
+}
+
+function update ()
+{
+
+}
