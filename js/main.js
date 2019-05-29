abde5dbe8216a457ce0e93ae492348c4fdb5a3b8
Thiebaud <contact@communnicasciences.eu>
2019-05-29 11:41:18 +0200

    creation joueur

--- js/main.js
+++ js/main.js
@@ -4,18 +4,32 @@ var config = {
     type: Phaser.AUTO,
     width:800,
     height:600,
+    scene : {
+            preload : preload,
+            create : create,
+            update : update,
+                    
+            
+    }
 };
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
+    
+        this.load.image('player', 'assets/player.png');
+        
+
 
 }
 
 function create ()
 {
+        var joueur = this.add.image(20, 300, 'player');
+        joueur.setScale (0.7);
 
+        
 }
 
 function update ()
